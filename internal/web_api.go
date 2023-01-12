6abebaac827c46f6d8b3d58ed14862a5ca3cd2b5
Maciej Wójcik <95306882+maciejjwojcik@users.noreply.github.com>
2023-01-12 12:42:57 +0100

    API-12106 allow custom logger v6 (#127)
    * API-12106 add custom logger
    
    * API-12106 change definition of default log
    
    * API-12106 add logging legacy/deprecated versions

--- internal/web_api.go
+++ internal/web_api.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"mime/multipart"
 	"net/http"
 	"time"
@@ -38,6 +39,7 @@ type api struct {
 	customHeaders         http.Header
 	retryStrategy         RetryStrategyFunc
 	statsSink             StatsSinkFunc
+	logger                *log.Logger
 }
 
 // HTTPRequestGenerator is called by each API method to generate api http url.
@@ -70,6 +72,7 @@ func NewAPI(t authorization.TokenGetter, client *http.Client, clientID string, r
 		httpEndpointGenerator: r,
 		customHeaders:         make(http.Header),
 		statsSink:             func(metrics.APICallStats) {},
+		logger:                log.Default(),
 	}, nil
 }
 
@@ -252,6 +255,13 @@ func (a *api) send(req *http.Request, respPayload interface{}) error {
 			return err
 		}
 
+		if h := resp.Header.Get("Legacy"); h != "" {
+			a.logger.Printf("[Notice] This is a legacy version. It will be deprecated after %s.", h)
+		}
+		if h := resp.Header.Get("Deprecation"); h != "" {
+			a.logger.Printf("[Warning] This version is deprecated. It will be decommissioned after %s.", h)
+		}
+
 		return json.Unmarshal(bodyBytes, respPayload)
 	}
 
@@ -280,4 +290,9 @@ func DefaultHTTPRequestGenerator(name string) HTTPEndpointGenerator {
 	return func(token *authorization.Token, host, action string) string {
 		return fmt.Sprintf("%s/v%s/%s/action/%s", host, apiVersion, name, action)
 	}
+}
+
+// SetLogger allows to set a custom logger. When it's not called, a default logger will be used.
+func (a *api) SetLogger(logger *log.Logger) {
+	a.logger = logger
 }
