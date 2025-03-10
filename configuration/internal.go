1b5db1765795767a8f2f809214d95c74e07a9dae
Damian Garbacz <dgarbacz@livechat-integrations.com>
2025-03-10 13:27:07 +0100

    feat: Fix update group properties typo.

--- configuration/internal.go
+++ configuration/internal.go
@@ -162,7 +162,7 @@ type updateLicensePropertiesRequest struct {
 }
 
 type updateGroupPropertiesRequest struct {
-	ID         int        `json:"id"`
+	GroupID    int        `json:"group_id"`
 	Properties Properties `json:"properties"`
 }
 
